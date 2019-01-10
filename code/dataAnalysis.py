2ecb0db5684a575e4d2bf3603602e63d36662cf4
David Dunn <qenopster@gmail.com>
2019-01-10 18:54:01 -0500

    updated to matplotlib

--- code/dataAnalysis.py
+++ code/dataAnalysis.py
@@ -9,56 +9,47 @@ import sys, os
 sys.path.append('./submodules')
 from psychopy import data, gui, core
 from psychopy.tools.filetools import fromFile
-import pylab
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 #Open a dialog box to select files from
 files = gui.fileOpenDlg('.')
 if not files:
     core.quit()
 
+#plot each staircase
+fig = Figure()
+canvas = FigureCanvas(fig)
+ax = fig.add_subplot(121)
+colors = 'brgkcmbrgkcm'
+lines, names = [],[]
 #get the data from all the files
-allIntensities, allResponses, labels = [],[], []
 for thisFileName in files:
     thisDat = fromFile(thisFileName)
     if isinstance(thisDat, data.staircase.MultiStairHandler):
         for handler in thisDat.staircases:
-            allIntensities.append( handler.intensities )
-            allResponses.append( handler.data )
-            labels.append(handler.condition['label'])
-    else:
-        allIntensities.append( thisDat.intensities )
-        allResponses.append( thisDat.data )
-        labels.append(thisFileName)
-
-#plot each staircase
-pylab.subplot(121)
-colors = 'brgkcmbrgkcm'
-lines, names = [],[]
-for labelN, thisStair in enumerate(allIntensities):
-    #lines.extend(pylab.plot(thisStair))
-    #names = labels[labelN]
-    pylab.plot(thisStair, label=labels[labelN])
-
-pylab.legend()
-
-'''#get combined data
-combinedInten, combinedResp, combinedN = \
-             data.functionFromStaircase(allIntensities, allResponses, 5)
-#fit curve - in this case using a Weibull function
-fit = data.FitWeibull(combinedInten, combinedResp, guess=[0.2, 0.5])
-smoothInt = pylab.arange(min(combinedInten), max(combinedInten), 0.001)
-smoothResp = fit.eval(smoothInt)
-thresh = fit.inverse(0.8)
-print(thresh)
-
-#plot curve
-pylab.subplot(122)
-pylab.plot(smoothInt, smoothResp, '-')
-pylab.plot([thresh, thresh],[0,0.8],'--'); pylab.plot([0, thresh],\
-[0.8,0.8],'--')
-pylab.title('threshold = %0.3f' %(thresh))
-#plot points
-pylab.plot(combinedInten, combinedResp, 'o')
-pylab.ylim([0,1])
+            ax.plot(handler.intensities, label=handler.condition['label'])
+            ax.plot(handler.data)
 '''
-pylab.show()
+            combinedInten, combinedResp, combinedN = \
+             data.functionFromStaircase(handler.intensities, handler.data, 10)
+            #fit curve - in this case using a Weibull function
+            fit = data.FitWeibull(combinedInten, combinedResp, guess=[0.2, 0.5])
+            smoothInt = pylab.arange(min(combinedInten), max(combinedInten), 0.001)
+            smoothResp = fit.eval(smoothInt)
+            thresh = fit.inverse(0.8)
+            print(thresh)
+
+            #plot curve
+            pylab.subplot(122)
+            pylab.plot(smoothInt, smoothResp, '-')
+            pylab.plot([thresh, thresh],[0,0.8],'--'); pylab.plot([0, thresh],\
+            [0.8,0.8],'--')
+            pylab.title('threshold = %0.3f' %(thresh))
+            #plot points
+            pylab.plot(combinedInten, combinedResp, 'o')
+            pylab.ylim([0,1])
+            '''
+            
+ax.legend()
+canvas.print_figure
